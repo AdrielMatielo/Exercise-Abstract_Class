6448e6642bbe8e37171d273e43a7047243f0e376
AdrielMatielo <adrielcmmatielo@gmail.com>
2020-07-26 22:29:07 -0300

    Entitie Company created

---
+++ src/entities/Company.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class Company extends Taxpayer{
+	
+	private Integer numberOfEmployees;
+	
+	public Company() {
+		super();
+	}
+	
+	public Company(String name, Double anualIncome, Integer numberOfEmployees) {
+		super(name, anualIncome);
+		this.numberOfEmployees = numberOfEmployees;
+	}
+	
+	public Integer getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+
+	public void setNumberOfEmployees(Integer numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
+
+	@Override
+	public double taxToPay() {
+		if (numberOfEmployees > 10 ) {
+			return getAnualIncome()*0.14;
+		}else {
+			return getAnualIncome()*0.16;
+		}
+	}
+
+}
